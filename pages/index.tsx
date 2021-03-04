426eb9a7413743f9f0c2b2dfbae17949a1597a13
hanahem <wawa.h8@gmail.com>
2021-03-04 14:03:42 +0100

    :lipstick: Setting up the header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link'
+import React from 'react';
+import Image from 'next/image'
 
 const IndexPage = () => (
   <div>
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <div className={`flex flex-col items-center content-center m-auto text-center mt-20`}>
+      <Image src={`/images/logo.png`} alt={"Kiwi"} height={'70px'} width={'70px'}/>
+      <span className={`text-4xl`}>Welcome to Kiwi dApp👋</span>
+      <p>A site to test <code className={`bg-green-100`}>web3.js</code> and Metamask wallets.</p>
+    </div>
   </div>
 )
 
