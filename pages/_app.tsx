c8f1b680606207bd8e3d07c5e60be1dcc337b948
hanahem <wawa.h8@gmail.com>
2021-03-04 17:04:59 +0100

    :egg: Setting up redux simple architecture

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import {AppProps} from "next/app";
 import "../style.css";
+import {Provider} from "react-redux";
+import {useStore} from "../store/store";
 
-function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  const store = useStore(pageProps.initialReduxState);
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
-
-export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Image from 'next/image'
 import Wallet from '../components/Wallet/Wallet';
-import Web3 from "web3";
 
 declare global {
-  interface Window { web3: any; ethereum: any; }
+  interface Window {
+    web3: any;
+    ethereum: any;
+  }
 }
 
 if (typeof window !== "undefined") {
@@ -14,22 +16,24 @@ if (typeof window !== "undefined") {
   //Check function to see if the MetaMask extension is installed
   const isMetaMaskInstalled = () => {
     //Have to check the ethereum binding on the window object to see if it's installed
-    const { ethereum } = window;
+    const {ethereum} = window;
     return Boolean(ethereum && ethereum.isMetaMask);
   };
 
 
 }
 
-const IndexPage = () => (
-  <div>
-    <div className={`flex flex-col items-center content-center m-auto text-center mt-20`}>
-      <Image src={`/images/logo.png`} alt={"Kiwi"} height={'70px'} width={'70px'}/>
-      <span className={`text-4xl`}>Welcome to Kiwi dApp👋</span>
-      <p className={`font-light`}>An app to test <code className={`bg-green-100`}>web3.js</code> and Metamask integrations.</p>
-      <Wallet/>
-    </div>
-  </div>
-)
+
+const IndexPage = () => {
+  return (
+      <div className={`flex flex-col items-center content-center m-auto text-center mt-20`}>
+        <Image src={`/images/logo.png`} alt={"Kiwi"} height={'70px'} width={'70px'}/>
+        <span className={`text-4xl`}>Welcome to Kiwi dApp👋</span>
+        <p className={`font-light`}>An app to test <code className={`bg-green-100`}>web3.js</code> and Metamask
+          integrations.</p>
+        <Wallet/>
+      </div>
+  );
+}
 
 export default IndexPage
