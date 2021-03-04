b755c442d4986d2b2b83b0f764f937b99cb6668c
hanahem <wawa.h8@gmail.com>
2021-03-04 11:48:36 +0100

    Setting up next, TS and tailwindcss

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 }
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+  <div className={`w-1/2 bg-red-500 m-auto`}>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          100: '#ee8133',
+          500: '#cbd37a',
+        },
+        primary: {
+          100: '#98405f',
+          500: '#62293d',
+        },
+        background: '#ebe6f7',
+      },
+      borderRadius: {
+        DEFAULT: '.5rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
