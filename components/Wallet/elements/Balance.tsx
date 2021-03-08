f6f3d6331c6c5d10ec2673f78bf2c2ef3ff175aa
hanahem <wawa.h8@gmail.com>
2021-03-08 17:08:57 +0100

    :sparkles: Integrating coingecko API

--- components/Wallet/elements/Balance.tsx
+++ components/Wallet/elements/Balance.tsx
@@ -1,11 +1,31 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import Box from "../../modules/Box";
+import {CoinGeckoAPI} from '@coingecko/cg-api-ts';
 
 type Props = {
   balance: string;
 }
 
 const Balance: FC<Props> = ({balance}) => {
+
+  const [eurPrice, setEurPrice] = useState<number | undefined>();
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      const fetch = window.fetch.bind(window);
+      const cg = new CoinGeckoAPI(fetch);
+
+      cg.getSimplePrice(["ethereum"], ["eur"]).then((res) => {
+        if (res) {
+          setEurPrice(parseFloat(balance) * (res.data.ethereum.eur as number));
+          console.log(res);
+        } else {
+          console.error("GC server not responding properly");
+        }
+      });
+    }
+  }, [balance]);
+
   return (
     <>
       {!
@@ -15,7 +35,9 @@ const Balance: FC<Props> = ({balance}) => {
         <div className={`w-full flex`}>
           <div className={`mr-2 w-1/2`}>
             <Box>
-              {parseInt(balance).toFixed(6) + ' ETH'}
+              <p className={`text-md text-gray-500`}>{'Total Assets'}</p>
+              <p className={`text-3xl`}>{(eurPrice as number).toFixed(2)} €</p>
+              <p className={`text-sm font-light text-gray-900`}>{parseFloat(balance).toFixed(8)} ETH</p>
             </Box>
           </div>
           <div className={`ml-2 w-1/2`}>
