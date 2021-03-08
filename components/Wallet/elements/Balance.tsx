3c23aba4d48ea8ee6156e80fbb821fae348e4ef3
hanahem <wawa.h8@gmail.com>
2021-03-08 17:56:53 +0100

    :lipstick: Setting up assets UI

--- components/Wallet/elements/Balance.tsx
+++ components/Wallet/elements/Balance.tsx
@@ -1,6 +1,30 @@
 import React, {FC, useEffect, useState} from 'react';
 import Box from "../../modules/Box";
 import {CoinGeckoAPI} from '@coingecko/cg-api-ts';
+import AssetBox from "../../modules/AssetBox";
+
+const assets = [
+  {
+    id: 'eth',
+    balance: 0.00434521,
+    ticker: 'ETH'
+  },
+  {
+    id: 'usdc',
+    balance: 22.30,
+    ticker: 'USDC'
+  },
+  {
+    id: 'wbtc',
+    balance: 0.000021,
+    ticker: 'wBTC'
+  },
+  {
+    id: 'other',
+    balance: 0.00032312,
+    ticker: 'Other ERC20'
+  },
+]
 
 type Props = {
   balance: string;
@@ -18,7 +42,6 @@ const Balance: FC<Props> = ({balance}) => {
       cg.getSimplePrice(["ethereum"], ["eur"]).then((res) => {
         if (res) {
           setEurPrice(parseFloat(balance) * (res.data.ethereum.eur as number));
-          console.log(res);
         } else {
           console.error("GC server not responding properly");
         }
@@ -32,18 +55,18 @@ const Balance: FC<Props> = ({balance}) => {
         balance.length ? (
         <p className={`font-light mt-12`}>{`Your balance is not available yet.`}</p>
       ) : (
-        <div className={`w-full flex`}>
-          <div className={`mr-2 w-1/2`}>
+        <div className={`my-6 w-full flex flex-col`}>
+          <div className={`mr-2 w-full`}>
             <Box>
               <p className={`text-md text-gray-500`}>{'Total Assets'}</p>
               <p className={`text-3xl`}>{(eurPrice as number).toFixed(2)} €</p>
               <p className={`text-sm font-light text-gray-900`}>{parseFloat(balance).toFixed(8)} ETH</p>
             </Box>
           </div>
-          <div className={`ml-2 w-1/2`}>
-            <Box>
-              aa
-            </Box>
+          <div className={`ml-2 py-2 w-full grid grid-flow-col grid-cols-2 grid-rows-2 gap-4`}>
+            {assets.map((asset) => (
+              <AssetBox id={asset.id} balance={asset.balance} ticker={asset.ticker}/>
+            ))}
           </div>
         </div>
       )}
