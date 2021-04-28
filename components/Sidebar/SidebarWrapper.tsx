e80338abab16e2ae4a2dd9553e5e85a14f15ea4d
hanahem <wawa.h8@gmail.com>
2021-04-28 22:22:26 +0200

    :lipstick: Updating CSS layout

--- components/Sidebar/SidebarWrapper.tsx
+++ components/Sidebar/SidebarWrapper.tsx
@@ -9,11 +9,11 @@ const SidebarWrapper: FC<{ children: ReactNode }> = ({ children }) => {
   const router = useRouter();
   const { pathname } = router;
   return (
-    <div className="bg-bg flex min-h-screen h-full w-full">
-      <nav className="bg-dark w-20 h-full flex flex-col items-center justify-between py-2">
+    <div className="bg-bg flex min-h-screen w-full">
+      <nav className="bg-dark w-20 h-screen fixed flex flex-col items-center justify-between py-2">
         <div>
           <div className="sidebtn" onClick={() => router.push("/")}>
-            <img src="/images/turbo.svg" alt="Sidebar icon" className="p-4" />
+            <img src="/images/turbo.svg" alt="Sidebar icon" className="p-2" />
           </div>
           <div className="h-1 w-1/2 bg-white rounded-sm my-4 mx-auto" />
           <div className={pathname === "/home" ? "sidebtn-active" : "sidebtn"}>
