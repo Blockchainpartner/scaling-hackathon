2d2aaf65ee8bb1fcdcee21d9a13b50ac30bea1b2
hanahem <wawa.h8@gmail.com>
2021-04-28 14:42:49 +0200

    :lipstick: Adding Profile on login

--- components/Torus.tsx
+++ components/Torus.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import OpenLogin from "@toruslabs/openlogin";
 import useClientEffect from "../utils/useClientEffect";
+import Torus from "@toruslabs/torus-embed";
 
 const VERIFIER = {
   loginProvider: "google",
@@ -21,22 +22,39 @@ const notLoggedIn = () => {
   );
 };
 
-const profile = (privKey: string, onLogout: () => void) => {
+const profile = (account: any, privKey: string, onLogout: () => void) => {
   return (
     <div className="flex items-start p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
-      <div className="circle bg-blue-500 mr-4">
-        <span />
-      </div>
-      <div>
-        <h3 className="text-2xl font-bold">Profile &rarr;</h3>
-        <p className="mt-4 text-sm break-all">{privKey}</p>
-        <p
-          className="mt-4 text-xs text-right underline cursor-pointer text-red-500 hover:text-red-500"
-          onClick={onLogout}
-        >
-          Logout
-        </p>
-      </div>
+      {account ? (
+        <img src={account.profileImage} alt={account.name} className="mr-4" />
+      ) : (
+        <div className="circle bg-blue-500 mr-4">
+          <span />
+        </div>
+      )}
+      {account ? (
+        <div>
+          <h3 className="text-2xl font-bold">{account.name} &rarr;</h3>
+          <p className="mt-1 text-sm break-all">{account.email}</p>
+          <p
+            className="mt-4 text-xs text-right underline cursor-pointer text-red-500 hover:text-red-500"
+            onClick={onLogout}
+          >
+            Logout
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h3 className="text-2xl font-bold">Private key &rarr;</h3>
+          <p className="mt-4 text-sm break-all">{privKey}</p>
+          <p
+            className="mt-4 text-xs text-right underline cursor-pointer text-red-500 hover:text-red-500"
+            onClick={onLogout}
+          >
+            Logout
+          </p>
+        </div>
+      )}
     </div>
   );
 };
@@ -45,11 +63,13 @@ const TorusInit: FC = () => {
   const [isLoading, setLoading] = useState(true);
   const [openlogin, setOpenLogin] = useState<undefined | OpenLogin>();
   const [privKey, setPrivKey] = useState<undefined | string>();
-  const [userInfo, setUserInfo] = useState<any>();
+  const [torus, setTorus] = useState<any>();
+  const [account, setAccount] = useState<any>();
 
   useClientEffect(() => {
     (async function () {
       try {
+        //OpenLogin
         const openlogin = new OpenLogin({
           clientId: VERIFIER.clientId,
           iframeUrl: "http://beta.openlogin.com",
@@ -58,6 +78,13 @@ const TorusInit: FC = () => {
         setOpenLogin(openlogin);
         await openlogin.init();
         setPrivKey(openlogin.privKey);
+
+        //Torus Wallet
+        const torus = new Torus({});
+        await torus.init({
+          enableLogging: false,
+        });
+        setTorus(torus);
       } finally {
         setLoading(false);
       }
@@ -89,14 +116,32 @@ const TorusInit: FC = () => {
     }
   };
 
-  const getProfile = async () => {};
+  const onTorusLogin = async () => {
+    if (isLoading || !torus) return;
+    await torus?.login({ verifier: VERIFIER.loginProvider });
+  };
 
-  console.log("FIN", openlogin);
+  //Grab account when logged in
+  useEffect(() => {
+    (async function () {
+      if (torus && torus?.isLoggedIn) {
+        const userInfo = await torus.getUserInfo();
+        setAccount(userInfo);
+      }
+    })();
+  }, [torus]);
+
+  //   const getProfile = async () => {};
+
+  console.log("OL", openlogin);
+  console.log("PK", privKey);
+  console.log("ACC", account);
+  console.log("TORUS", torus);
 
   if (isLoading) return <div className="animate-pulse text-3xl">⏳</div>;
-  return privKey ? (
+  return privKey || torus?.isLoggedIn ? (
     <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-      {profile(privKey as string, onLogout)}
+      {profile(account, privKey as string, onLogout)}
     </div>
   ) : (
     <div>
@@ -107,9 +152,9 @@ const TorusInit: FC = () => {
         >
           OpenLogin
         </button>
-		<button
+        <button
           className="bg-blue-600 text-white py-2 px-3 rounded-md ml-2"
-          onClick={onLogin}
+          onClick={onTorusLogin}
         >
           Torus Wallet
         </button>
