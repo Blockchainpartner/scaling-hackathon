bf2205d1bd4f33e311b16d49dd19cc93097f493e
hanahem <wawa.h8@gmail.com>
2021-05-07 00:36:23 +0200

    :sparkles: Introducing disability info field

--- pages/identity.tsx
+++ pages/identity.tsx
@@ -239,6 +239,7 @@ function  ExistingUserProfil() {
 								info={`${user.KYC.location.city}, ${user.KYC.location.state}`}
 							/>
 							<IdInfoItem label="Country" info={user.KYC.location.country} />
+							<IdInfoItem label="Disabled" info={user.KYC.disabled ? "Yes" : "No"} />
 						</div>
 					</div>
 				</div>
